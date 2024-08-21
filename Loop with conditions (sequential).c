04fc88f24a17b9a00da9f824a48f6d8a0762121f
Shahriar Faysal <160359045+shahriarfaysal@users.noreply.github.com>
2024-08-22 02:27:32 +0600

    Create Loop with conditions (sequential).c

---
+++ Loop with conditions (sequential).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int i,n;
+  scanf("%d", &n);
+  for(i=1;i<=n;i++)
+  {
+      if(i%2==0)
+      {
+          printf("%d-Even\n", i);
+      }
+      else
+      {
+          printf("%d-Odd\n", i);
+      }
+  }
+    return 0;
+}
